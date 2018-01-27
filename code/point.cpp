f7c799cc2cd490b9a9a43ddec7a02dc0a5181777
Pedro Sousa <pedroteosousa@gmail.com>
2018-01-27 09:38:43 -0200

    updated point struct

--- code/point.cpp
+++ code/point.cpp
@@ -1,49 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef double type;
-double EPS = 1e-12;
+double EPS = 1e-8;
 
 struct point {
-    type x, y;
-    point(type xp = 0.0, type yp = 0.0) {
-        x = xp;
-        y = yp;
-    }
-    point(const point &p) {
-        x = p.x;
-        y = p.y;
-    }
+    double x, y;
+    
+	point (double xp = 0, double yp = 0) { x = xp; y = yp;}
+    point (const point &p) { x = p.x; y = p.y; }
+
     point operator+ (const point &p) const { return point(x+p.x, y+p.y); }
     point operator- (const point &p) const { return point(x-p.x, y-p.y); }
-    point operator* (type c) { return point(c*x, c*y); }
-    point operator/ (type c) { return point(x/c, y/c); }
+    point operator* (double c) const { return point(c*x, c*y); }
+    point operator/ (double c) const { return point(x/c, y/c); }
 
-    bool operator<(const point &p) {
+	double operator* (const point &p) const { return x*p.x+y*p.y; }
+	double operator^ (const point &p) const { return x*p.y-y*p.x; }
+	
+	double len () const { return hypot(x, y); }
+    
+	bool operator< (const point &p) {
 		return x < p.x || x == p.x && y < p.y;
 	}
 };
-
-type dot(point p, point q)  { return p.x*q.x+p.y*q.y; }
-type dist(point p, point q)  { return sqrt(dot(p-q,p-q)); }
-type cross(point p, point q)  { return p.x*q.y-p.y*q.x; }
-
-point projectInLine(point c, point a, point b) {
-	return a + (b-a)*dot(c-a, b-a)/dot(b-a, b-a);
-}
-
-point projectInSegment(point c, point a, point b) {
-    point lineP = projectInLine(c, a, b);
-    type maxDist = max(dist(a, lineP), dist(b, lineP));
-    if (maxDist > dist(a, b)) {
-        if (dist(a, c) > dist(b, c)) return b;
-        else return a;
-    }
-    else return lineP;
-}
-
-int main() {
-	point a(0, 0), b(1, 1), c(1, 0);
-	c = projectInSegment(c, a, b);
-	printf("%lf %lf\n", c.x, c.y);
-}

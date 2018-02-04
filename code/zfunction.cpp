20f5e5d2e2de3e6e60f59c9e8eadc6fcc10d4c94
pedroteosousa <pedroteosousa@gmail.com>
2018-02-04 21:06:29 -0200

    added zfunction and a few tests

---
+++ code/zfunction.cpp
@@ -0,0 +1,15 @@
+int z[N];
+
+void Z(const char *s) {
+	int n = strlen(s);
+	int m = -1;
+	for (int i = 1; i < n; i++) {
+		z[i] = 0;
+		if (m != -1 && m + z[m] >= i)
+			z[i] = min(m + z[m] - i, z[i-m]);
+		while (i + z[i] < n && s[i+z[i]] == s[z[i]])
+			z[i]++;
+		if (m == -1 || i + z[i] > m + z[m])
+			m = i;
+	}
+}
---
+++ tests/zfunction.tests.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include "gtest/gtest.h"
+using namespace std;
+
+const int N = 1e5;
+#include "../code/zfunction.cpp"
+
+void brutao(string s) {
+	Z(s.c_str());
+	for (int i = 1; i < (int)s.size(); i++) {
+		int match = 0;
+		while (i + match < (int)s.size() && s[i + match] == s[match]) match++;
+		EXPECT_EQ(z[i], match);
+	}
+}
+
+TEST(Simple, Random) {
+	int num_tests = 10, alf = 26, n = 10000;
+	while (num_tests--) {
+		string s;
+		while ((int)s.size() < n) {
+			s.push_back('a' + (rand()%alf));
+			brutao(s);
+		}
+	}
+}

7f487e24371765c47ed57114cdf66db438efc796
Pedro Sousa <pedroteosousa@gmail.com>
2018-02-06 18:38:37 -0200

    fixed bug where KMP would not match first character

--- code/kmp.cpp
+++ code/kmp.cpp
@@ -13,7 +13,7 @@ vector<int> match(const char *p, const char *s) {
 	int n = strlen(s), m = strlen(p), j = 0;
 	vector<int> matches;
 	for (int i = 1; i < n+1; i++) {
-		while (j >= 0 && p[j] != s[i]) j = kmp[j];
+		while (j >= 0 && p[j] != s[i-1]) j = kmp[j];
 		if (++j == m) {
 			matches.push_back(i-j+1);
 			j = kmp[j];
